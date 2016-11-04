0492a78580b43896f2fae6559a80c2779c0a5759
Grace Han <hanpanpan@Graces-MacBook-Pro.local>
2016-11-04 17:39:50 +0800

    feat: generate thumbnail image from pdf and save

--- app/uploaders/preview_image_uploader.rb
+++ app/uploaders/preview_image_uploader.rb
@@ -4,7 +4,7 @@ class PreviewImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -32,9 +32,20 @@ class PreviewImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :thumbnail do
+    process :thumbnail_pdf
+    process :resize_to_fit => [100, 50]
+    process :convert => :jpg
+    def full_filename (for_file = model.source.file)
+      super.chomp(File.extname(super)) + '.jpg'
+    end
+  end
+
+  def thumbnail_pdf
+    manipulate! do |frame, index|
+      frame if index.nil?
+    end
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
